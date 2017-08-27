dc106d1e6a70584802634ec7a1b0a2dfe60d82e0
Matthew Schmidt <schmidty99203@gmail.com>
2017-08-27 11:35:56 -0700

    added working unit test, favicon to index view

--- index.php
+++ index.php
@@ -5,9 +5,13 @@ require_once "vendor/autoload.php";
 use App\Salad;
 
 
+echo sprintf( "<!DOCTYPE html>\n<head>\n<title></title>\n<link rel='icon' type='image/x-icon' href='http://{$_SERVER['HTTP_HOST']}/desktop.ico' />\n</head>\n<body>\n" );
+
 $greekSalad = new Salad( ['Romain lettuce', 'Onion', 'Black Olives', 'Green Bell pepper', 'Red Bell pepper', 'Tomato', 'Cucumber', 'Feta Cheese', 'Olive Oil', 'Oregano'] );
 
 foreach( $greekSalad as $ingredient ) {
 	echo sprintf( "%s<br>\n", $ingredient );
 }
  
+
+echo sprintf( "</body>\n" );
