ede4231ff7801a3195f218c53a0a5400dd83b75c
Matthew Schmidt <schmidty99203@gmail.com>
2017-08-27 19:18:37 -0700

    added database connection

--- src/Salad.php
+++ src/Salad.php
@@ -1,15 +1,45 @@
 <?php
 namespace App;
 
+use Symfony\Component\Yaml\Parser;
+
 
 class Salad implements \IteratorAggregate
 {
-	protected $ingredients;
+	protected
+		$db,
+
+		$ingredients,
+
+		$yaml
+	;
 
 
 	public function __construct( array $ingredients )
 	{
+		$this->yaml = new Parser();
+		$this->getMySQLDbConnection();
+
 		$this->ingredients = $ingredients;
+	}
+
+	private function getDbConfigFile()
+	{
+		if ( !file_exists( 'database.yml' ) ) {
+			throw new \Exception('database config not found');
+		}
+		return $this->yaml->parse( file_get_contents( 'database.yml' ) );
+	}
+
+	private function getMySQLDbConnection()
+	{
+		$yaml = $this->getDbConfigFile()['mysql'];
+		$this->db = new \PDO('mysql:host='. $yaml['dbhost'] .';dbname='. $yaml['dbname'], $yaml['dbuser'], $yaml['dbpass']);
+	}
+
+	public function getDb()
+	{
+		return $this->db;
 	}
 
 	public function getIterator()
